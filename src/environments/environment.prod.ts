36c4d13d05580fbbdf0634b4c770e8f40256231a
mai.ngo.intern <mai.ngo.intern@ntq-solution.com.vn>
2021-04-04 21:25:17 +0700

    add env file

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCbXHKtnrlVPdT4yz5dAEYrPbbyONFcpjw',
+    authDomain: 'angular-demo-0811.firebaseapp.com',
+    projectId: 'angular-demo-0811',
+    storageBucket: 'angular-demo-0811.appspot.com',
+    messagingSenderId: '720177793061',
+    appId: '1:720177793061:web:f3cb231576167eec2b57cc'
+  }
+};
